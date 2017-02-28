5bca6cbaf8a6e9cb13cd0d2a856e55c48dcf11f6
ADI LASYLVA <adillasylva126@gmail.com>
2017-02-28 15:44:09 +0700

    Add files via upload

---
+++ Pertemuan 2/Mahasiswa.java
@@ -0,0 +1,8 @@
+/*Program Minggu Ke-2*/
+class Mahasiswa{
+	public static void main(String[] args) {
+		System.out.println("Nama   : Adi Lasylva\n");
+		System.out.println("NIM    : 10115258\n");
+		System.out.println("Alamat : Majalengka\n");
+		}
+}
